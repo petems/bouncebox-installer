917118a5c761d6a46bca8cbe31128f649a5d2b3a
Peter Souter <p.morsou@gmail.com>
2014-02-08 19:17:52 +0000

    First run at using Kafo to make a basic bouncebox

---
+++ modules/bouncebox/manifests/init.pp
@@ -0,0 +1,30 @@
+# This adds the basebones for a basic bouncebox
+# Right now thats: Irrisi and mosh
+#
+# == Parameters
+#
+#
+# == Examples
+#
+#   include bouncebox
+#
+#   class { 'bouncebox':}
+#
+# == Authors
+#
+# Peter M Souter
+#
+# == Copyright
+#
+# Copyright 2014
+class bouncebox {
+
+  package { 'mosh':
+    ensure => installed,
+  }
+
+  package { 'irssi':
+    ensure => installed,
+  }
+
+}
